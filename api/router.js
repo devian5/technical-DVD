d8f0447e9b48ac34bbab8cffb3d153055b583091
devian5 <acgarage@outlook.es>
2021-05-11 20:09:37 +0200

    Created admin controller and router with the method create

--- api/router.js
+++ api/router.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRouter = require('./routers/user.router');
 const movieRouter = require('./routers/movie.router');
 const orderRouter = require('./routers/order.router');
+const adminRouter = require('./routers/admin.router');
 
 router.use('/user', userRouter);
 router.use('/movie', movieRouter);
 router.use('/order', orderRouter);
+router.use('/admin-club', adminRouter);
 
 module.exports = router;
